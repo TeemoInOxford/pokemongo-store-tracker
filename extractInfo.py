4a494882e20cbb7cb084a4aedb0d1ecc05f77882
TeemoInOxford <54721469+TeemoInOxford@users.noreply.github.com>
2025-06-03 02:55:47 +0800

    Update extractInfo.py

--- extractInfo.py
+++ extractInfo.py
@@ -132,7 +132,7 @@ for category in categories[1:]:
             "name": full_name,
             "endTimeMs": convert_time(end_time),
             "bundledItems": bundle_info,
-            "sellingPrice": f"{selling_price} RMB",
+            "sellingPrice": f"{selling_price} CNY 人民币",
             "price": price,
             "imageUrl": imageUrl
         })
